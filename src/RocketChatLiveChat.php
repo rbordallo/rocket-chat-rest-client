0aaa3e9cbafbe21bc4efafa68db9565b7e11bc59
rbordallo <raulbordallo@gmail.com>
2019-05-27 12:33:53 +0200

    Refactorized API call and removed HTML from the return method.

--- src/RocketChatLiveChat.php
+++ src/RocketChatLiveChat.php
@@ -14,6 +14,7 @@ class LiveChat extends Client {
   public $visitor_username;
   public $visitor_token;
   public $visitor_status;
+  public $room_update;
   public $messages;
 
   public function __construct($room, $agent){
@@ -26,27 +27,36 @@ class LiveChat extends Client {
       $this->visitor_username = $room->v->username;
       $this->visitor_token = $room->v->token;
       $this->visitor_status = $room->v->status;
+      $this->room_update = $room->_updatedAt;
       $this->messages = [];
     }
   }
 
   public function messages_history($user_id, $user_auth_token) {
 
+    $ordered_messages = [];
+
     $headers = ['X-Auth-Token' => $user_auth_token, 'X-User-Id' => $user_id];
 
-    $url_parameters = $this->id . '?token=' . $this->visitor_token;
-    $response = Request::get( $this->api . 'livechat/messages.history/' . $url_parameters)->addHeaders($headers)->send();
+    if (isset($this->id) && isset($this->visitor_token)) {
+      $url_parameters = $this->id . '?token=' . $this->visitor_token;
+      $response = Request::get($this->api . 'livechat/messages.history/' . $url_parameters)
+        ->addHeaders($headers)
+        ->send();
 
-    $messages = [];
+      $messages = [];
 
-    if( $response->code == 200 && isset($response->body->success) && $response->body->success == true ) {
-      foreach($response->body->messages as $message) {
-        // $response->body->messages[1]->ts
-        $messages[] = $message->u->name . ': '. $message->msg;
+      if ($response->code == 200 && isset($response->body->success) && $response->body->success == TRUE) {
+        foreach ($response->body->messages as $message) {
+          $message_name = isset($message->alias) ? $message->alias : $message->u->username;
+          $messages[] = ["message_name" => $message_name, "message_msg" => $message->msg, "message_time" => $message->ts];
+        }
       }
+
+      $ordered_messages = array_reverse($messages);
     }
 
-    return $messages;
+    return $ordered_messages;
 
   }
 
